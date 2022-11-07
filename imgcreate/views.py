eb4742d81b162ca3723ca5ff10cbba9354e25a6d
ofcoursenp <nexusalt33@gmail.com>
2022-11-07 21:16:43 +0545

    Responsive website , 3 Variation of images

---
+++ imgcreate/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render,HttpResponse     
+from . import main   
+
+# Create your views here.
+
+def index(req):
+    if req.POST.get('what'):
+        main.openloader(what=req.POST.get('what'))
+    return render(req,'index.html',)
+
